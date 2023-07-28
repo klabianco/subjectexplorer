7921bbcb8efa6d4e06c7d9f957b049f936a2a6ea
Kevin Labianco <klabianco@gmail.com>
2023-07-28 08:47:10 -0700

    formatting update

--- inc/activity.php
+++ inc/activity.php
@@ -3,8 +3,8 @@
         border-radius: 10px;
         box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
     }
-    .card h2 {
-        color: #007BFF; /* This is a Bootstrap primary color. You can change as needed. */
+    .card h2.analysis-title {
+        color: #007BFF;
         font-weight: bold;
     }
     .card-body {
@@ -28,10 +28,10 @@
 <div class="container">
     <div class="card mt-4">
         <div class="card-body">
-            <h2>Your Analysis</h2>
+            <h2 class="analylsis-title">Your Analysis</h2>
             <div id="main-response"><?php echo $Activity->getResponse(); ?></div>
             <hr>
-            <?php if ($Activity->hasGrade()) : ?><p id="grade"><strong>Grade:</strong><?php echo $Activity->getGrade(); ?></p><?php endif; ?>
+            <?php if ($Activity->hasGrade()) : ?><p id="grade"><strong>Grade:</strong> <?php echo $Activity->getGrade(); ?></p><?php endif; ?>
             <?php if ($Activity->hasSubject()) : ?><p id="subject"><strong>Subject:</strong> <?php echo $Activity->getSubject(); ?></p><?php endif; ?>
             <p id="added-date"><strong>Date Added:</strong> <?php echo $Activity->getFormattedAddedDate(); ?></p>
         </div>
